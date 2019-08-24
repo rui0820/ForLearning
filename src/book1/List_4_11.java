df1734e843c074d831bf7e7d3f6cf8aa476edc02
rui0820 <karaku0820@gamil.com>
2019-08-24 09:59:00 +0800

    ?

--- src/book1/List_4_11.java
+++ src/book1/List_4_11.java
@@ -1,11 +1,11 @@
 package book1;
 
-public class List_4_9 {
+public class List_4_11 {
 
 	public static void main(String[] args) {
 		int[] score = {20,30,40,50,80};
-		for(int i = 0;i < score.length;i++) {
-			System.out.println(score[i]);
+		for(int value : score) {
+			System.out.println(value);
 		}
 	}
 }
